a6999c7ba23191587002bdf31164847f9b8c9291
Cooper Nichols <coopnich92@gmail.com>
2020-01-17 15:25:38 -0600

    fix undefine

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ let bandNumber = 0
 
 const takeNumber = function (bandName) {
   bandNumber++
-  console.log(`${bandNumber}. ${bandName}`)
+  return `${bandNumber}. ${bandName}`
 }
 
 const scum = takeNumber("Galactic Scum")
@@ -11,7 +11,7 @@ console.log(scum)  // This should print "1. Galactic Scum" in the console
 const under = takeNumber("Underdogs")
 console.log(under)  // This should print "2. Underdogs" in the console
 
-const genius = takeNumber("I'm a Fucking Genius")
+const genius = takeNumber("I'm a Genius")
 console.log(genius)
 
 const blah = takeNumber("Blah")
