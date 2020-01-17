16cba30d2622575ca4e58738bfc934e82e302231
Cooper Nichols <coopnich92@gmail.com>
2020-01-17 15:10:08 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+let bandNumber = 0
+
+const takeNumber = function (bandName) {
+  bandNumber++
+  console.log(`${bandNumber}. ${bandName}`)
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
+
+const genius = takeNumber("I'm a Fucking Genius")
+console.log(genius)
+
+const blah = takeNumber("Blah")
+console.log(blah)
